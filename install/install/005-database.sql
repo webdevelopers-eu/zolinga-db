2e8509580f096bdba810deb4e5755dd3f520364a
s1-p8-dev <s1-p8-dev@zolinga.org>
2026-06-13 16:39:54 +0200

    Add registry table for system-wide key-value store

---
+++ install/install/005-database.sql
@@ -0,0 +1,16 @@
+-- ----------------------------------------------------------------------------
+-- Registry (system-wide key-value store)
+-- ----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `registry` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Primary Key: Unique registry ID.',
+  `hash` binary(20) NOT NULL DEFAULT unhex(sha(`name`)) COMMENT 'unhex(sha1(name))',
+  `name` varchar(255) NOT NULL COMMENT 'Name of the registry item.',
+  `json` text NOT NULL COMMENT 'JSON serialized value of the registry item.',
+  `created` int(10) unsigned DEFAULT NULL COMMENT 'Auto updated by trigger. Date and time the registry item was created.',
+  `modified` int(10) unsigned DEFAULT NULL COMMENT 'Auto updated by trigger. Date and time the registry item was last modified.',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `hash` (`hash`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci COMMENT='Stores configuration data in a key-value pair format.';
+
+-- ========
